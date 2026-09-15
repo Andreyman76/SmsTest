67981ab41ed137640bc6d5fcecc7593f91d70687
Andreyman76 <kolbasa.1998@yandex.ru>
2026-09-15 17:37:29 +0700

    Добавлен mock gRPC сервер для тестов

---
+++ SmsTest.Server/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SmsTest.Server.Services;
+
+namespace FactoryMonitor.Server;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+        {
+            Args = args,
+            EnvironmentName = Environments.Development
+        });
+
+        builder.WebHost.ConfigureKestrel(options =>
+        {
+            options.ListenLocalhost(5000, listenOptions =>
+            {
+                listenOptions.Protocols =
+                    Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2;
+            });
+        });
+
+        builder.Services.AddGrpc();
+        builder.Services.AddGrpcReflection();
+
+        WebApplication app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapGrpcReflectionService();
+        }
+
+        app.MapGrpcService<SmsTestRpcServiceMockup>();
+
+        await app.RunAsync();
+    }
+}
